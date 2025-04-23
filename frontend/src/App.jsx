10339f9bb3ba8fecf80e6bd6de99a5e58305c60f
Lee Siang Yin <siangyin916@gmail.com>
2025-04-23 22:54:37 +0800

    setup project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+const App = () => {
+	return <div>MY APP</div>;
+};
+
+export default App;
